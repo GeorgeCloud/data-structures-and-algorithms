bbb60d05c8a4fc2fb5c5b541d4c97c39c64d8f6f
George Ceja <net.george.sky@gmail.com>
2018-03-29 15:31:26 -0700

    added test cases.

--- challenges/binary_search/test_binary_search.py
+++ challenges/binary_search/test_binary_search.py
@@ -1,12 +1,18 @@
-import pytest, time
+from binary_search import binary_search
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def test_binary_search_empty_list():
-	pass
+    """Validates if array is empty"""
+    assert binary_search([], 5) == -1
+
 
 def test_binary_search_returns_index():
-	pass
+    """Validates if value given returns the correct index of value in array."""
+    assert binary_search(arr, 7) == 6
+
 
 def test_binary_search_no_found():
-	pass
-
-
+    """Returns -1 if the value was not found, uses a binary search"""
+    assert binary_search([], 2) == -1
