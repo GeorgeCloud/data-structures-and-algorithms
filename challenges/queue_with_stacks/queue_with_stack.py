9b4cce53137bf31ddcbad776f9880f8d97f447e5
George Ceja <net.george.sky@gmail.com>
2018-04-02 23:23:58 -0700

    didnt know where to start

---
+++ challenges/queue_with_stacks/queue_with_stack.py
@@ -0,0 +1,14 @@
+from stack import Stack
+
+class Queue:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def enqueue(self):
+        pass
+
+    def dequeue(self):
+        pass
