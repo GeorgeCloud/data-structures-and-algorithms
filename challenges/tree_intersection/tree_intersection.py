2dc53a387ef07d9201c1e3acb998389d116fcdf9
George Ceja <net.george.sky@gmail.com>
2018-05-06 11:35:46 -0700

    Added Docstring and whiteboard.png

--- challenges/tree_intersection/tree_intersection.py
+++ challenges/tree_intersection/tree_intersection.py
@@ -1,6 +1,10 @@
 def tree_intersection(tree1, tree2):
+    """
+    INPUT  <--- (Binary Tree Object, Binary Tree Object)
+    OUTPUT <--- [ARRAY WITH NON-UNIQUE VALUES]
+    Joins only non-unique nodes from two binary trees.
+    """
     setA, setB = set(), []
-
     tree1.in_order(setA.add)
     tree2.in_order(setB.append)
     return [el for el in setB if el in setA]
