1effade833083f4ca30b7d1e51a0c960b9fd0bc2
George Ceja <net.george.sky@gmail.com>
2018-05-06 16:19:41 -0700

    Finished test cases and functionality of a joint hashtable.

--- challenges/left_join/left_join.py
+++ challenges/left_join/left_join.py
@@ -2,32 +2,18 @@ from hash_table import HashTable
 
 
 def left_join(table_1, table_2):
-    """Joins elements from table2 if their key is identical."""
-    left_joined = {}
-    for key in table_1:
-        left_joined[key] = [table_1.get(key), 'NULL']
-    for key in table_2:
-        if key in left_joined:
-            left_joined[key][1] = table_2.get(key)
-
-    return left_joined
-
-
-table1 = HashTable()
-table1.set('a', 5)
-tableFin = set()
-for i in table1.buckets:
-    if i.head is not None:
-        x = i.head.val
-        print(x)
-        # print(5 in i.head.val.values())
-
-# for k in table1.buckets:
-#     if i.head is not None and i.head.keys
-
-
-table2 = HashTable()
-table2.set('b', 6)
-# print(table2)
-
-# left_join(table1, table2)
+    """
+    No additional __iter__ for LinkedList or hash_table.
+    Joins elements from table2 if their key is identical.
+    """
+    new_hash = HashTable()
+    table1_keys = [list(keys1.head.val)[0] for keys1 in table_1.buckets if keys1.head is not None]
+    table2_keys = [list(keys2.head.val)[0] for keys2 in table_2.buckets if keys2.head is not None]
+    if not table1_keys:
+        return None
+    for x in table1_keys:
+        if x in table2_keys:
+            new_hash.set(x, [table_1.get(x), table_2.get(x)])
+        else:
+            new_hash.set(x, [table_1.get(x), None])
+    return new_hash
--- challenges/left_join/test_left_join.py
+++ challenges/left_join/test_left_join.py
@@ -4,21 +4,53 @@ from left_join import left_join
 
 
 @pytest.fixture
-def hash_table():
-    pass
+def empty_tables():
+    return HashTable(), HashTable()
 
 
-def test_invalid_hash_tables():
-    pass
+@pytest.fixture
+def empty_left_table():
+    rightTable = HashTable()
+    rightTable.set('a', 5)
+    return HashTable(), rightTable
+
+
+@pytest.fixture
+def empty_right_table():
+    leftTable = HashTable()
+    leftTable.set('a', 5)
+    return leftTable, HashTable()
+
+
+# SET UP FOR FIRST HASHTABLE
+hashtable1 = HashTable()
+hashtable1.set('a', 5)
+hashtable1.set('8', 523)
+
+# SET UP FOR SECOND HASHTABLE
+# SHARES ONE ELEMENT WITH HASHTABLE ONE
+hashtable2 = HashTable()
+hashtable2.set('z', 77)
+hashtable2.set('a', 63)
+
+
+def test_invalid_size_ht():
+    with pytest.raises(ValueError):
+        table = HashTable(-1)
+        # Does not reach here
+        return table
+
+
+def test_empty_left(empty_left_table):
+    """Validates None is returned if left table has no elements"""
+    assert left_join(empty_left_table[0], empty_left_table[1]) is None
+
+
+def test_empty_right(empty_right_table):
+    """Validates that the left is the only value present"""
+    assert left_join(empty_right_table[0], empty_right_table[1]).get('a') == [5, None]
 
 
 def test_left_simple_join():
-    pass
-
-
-def test_empty_left():
-    pass
-
-
-def test_empty_right(random_hash_table):
-    pass
+    """Validaes that two tables return the common key/values"""
+    assert left_join(hashtable1, hashtable2).get('a') == [5, 63]
